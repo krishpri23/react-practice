7f97b6147b077c8e5ac2dccfab3bccf5cb501be3
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-12-14 19:42:32 -0800

    initial commit

---
+++ shoppingcart/src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="mx-auto bg-green-200">
+      <h2 className="font-bold uppercase text-red-950"> hello </h2>
+    </div>
+  );
+}
+
+export default App;
--- useContext/src/App.tsx
+++ useContext/src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
+type Pokemon = {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+};
+
+// custom hook to  useContext
+function usePokemon() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("../pokemon.json")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+  }, []);
+
+  return { pokemon };
+}
+
 function App() {
+  const { pokemon } = usePokemon();
   return (
     <main>
-      <h2> Hello </h2>
+      <div>{JSON.stringify(pokemon)}</div>
     </main>
   );
 }
