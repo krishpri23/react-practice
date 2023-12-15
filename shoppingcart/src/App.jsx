6593527f0bbd838867674dac7c2d34e1b739b262
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-12-14 20:41:28 -0800

    added nav elements

--- shoppingcart/src/App.jsx
+++ shoppingcart/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
 import "./index.css";
+import Home from "./Home";
+import Cart from "./Cart";
 
 function App() {
   return (
-    <div className="mx-auto bg-green-200">
-      <h2 className="font-bold uppercase text-red-950"> hello </h2>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className=" w-full mx-auto bg-green-200">
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ shoppingcart/src/Header.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <nav className="bg-slate-700 px-6 py-3 flex justify-around text-white text-xl  items-center ">
+      Link
+      <h1> Shopping cart </h1>
+      <input
+        type="text"
+        placeholder="Search a product..."
+        className="px-5 py-1 rounded-md border-2 border-gray-600 w-72"
+      />
+      <button
+        className="cursor-pointer relative"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        {" "}
+        <FaShoppingCart />{" "}
+      </button>
+      {showMenu && (
+        <div className="bg-slate-50 border-2 border-gray-500 w-72 h-72 p-5 mt-2 absolute right-20 top-14 text-black">
+          <h1> cart is empty </h1>
+        </div>
+      )}
+    </nav>
+  );
+}
