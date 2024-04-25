581aef7efb15c64f9a439d160c277c07f522fef5
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-04-24 21:02:06 -0700

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,12 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-//layout
-import RootLayout, { RootLoader } from "./layouts/rootLayout";
-import CreateLayout from "./pages/signup";
-
-//screens
-import HomePage from "./pages/Home/homePage";
-import NotFound from "./pages/notFound";
-import SearchResults, {
-  loadSearchResults,
-} from "./pages/searchProvider/searchResults";
-import Login from "./pages/login";
-
-//styles
-import "./index.css";
-
-//utils
-import { loginAction, logoutAction, searchAction } from "./utils/actions";
-import Logout from "./pages/logout";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />} loader={RootLoader}>
-      <Route index element={<HomePage />} />
-      <Route
-        path="searchResults"
-        element={<SearchResults />}
-        action={searchAction}
-        loader={loadSearchResults}
-      />
-
-      <Route path="create" element={<CreateLayout />} />
-      <Route path="login" element={<Login />} action={loginAction} />
-      <Route path="logout" element={<Logout />} action={logoutAction} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
-  );
+  const [count, setCount] = useState(0);
+
+  return <div className="text-xl bg-emerald-600">hello</div>;
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@emotion/react'],
+  server: {
+    port: 5000,
   },
-  resolve: {
-    dedupe: ['@emotion/react']
-  }
-})
+});
